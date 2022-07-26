90048b62eb3ffad2ea4d98ac6e07bc7cc656c349
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-07-26 22:23:28 +0400

    Creating models

--- itfinder/projects/views.py
+++ itfinder/projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+#Все проекты
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+#Детальное представление
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-projects.html', {'project': projectObj})
